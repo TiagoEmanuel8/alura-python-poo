c5e5635dbca6a4dd82ac2f9e01ecdc2633ca0f89
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-08-19 20:19:10 -0300

    manipulação de strings

---
+++ app.py
@@ -0,0 +1,10 @@
+print('Sabor Express \n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativer restaurante')
+print('4. Sair \n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+# conceito de string literal
+print(f'Você escolheu a opção {opcao_escolhida}')
